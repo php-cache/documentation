656168e7fdf4350489d4836544cf604a742cf2ab
Malte Blättermann <malte.blaettermann@gmail.com>
2016-01-28 03:46:27 +0100

    ReRender with new settings for all formats

---
+++ logos/build.bat
@@ -0,0 +1,6 @@
+blender -b C:\cygwin64\home\malte\documentation\logos\php-cache-logo.blend -S Scene.512 -o //php-cache-logo-512.png -F PNG -f 1 
+blender -b C:\cygwin64\home\malte\documentation\logos\php-cache-logo.blend -S Scene.256 -o //php-cache-logo-256.png -F PNG -f 1  
+blender -b C:\cygwin64\home\malte\documentation\logos\php-cache-logo.blend -S Scene.128 -o //php-cache-logo-128.png -F PNG -f 1 
+blender -b C:\cygwin64\home\malte\documentation\logos\php-cache-logo.blend -S Scene.64  -o //php-cache-logo-64.png -F PNG -f 1 
+blender -b C:\cygwin64\home\malte\documentation\logos\php-cache-logo.blend -S Scene.32  -o //php-cache-logo-32.png -F PNG -f 1  
+blender -b C:\cygwin64\home\malte\documentation\logos\php-cache-logo.blend -S Scene.16  -o //php-cache-logo-16.png -F PNG -f 1 
